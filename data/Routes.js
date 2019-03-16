6563c66e14bd10ee9eb3c09debb483c76ecdcf44
mustafa inaya <moose.inaya@icloud.com>
2019-03-15 20:14:01 -0700

    UPDATE.

--- data/Routes.js
+++ data/Routes.js
@@ -27,4 +27,31 @@ router.get('/:id', async (req, res) => {
 		res.status(500).json({ error: 'The post information could not be retrieved.' });
 	}
 });
+router.post('/', async (req, res) => {
+	try {
+		const post = await Posts.add(req.body);
+		res.status(201).json(post);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			message: 'Error adding the post'
+		});
+	}
+});
+router.delete('/:id', async (req, res) => {
+	try {
+		const count = await post.remove(req.params.id);
+		if (count > 0) {
+			res.status(200).json({ message: 'The post has been deleted' });
+		} else {
+			res.status(404).json({ message: 'The post could not be found' });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			message: 'Error removing the post'
+		});
+	}
+});
+
 module.exports = router;
